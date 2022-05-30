fa3982ea71b4eeb7b7a418c2de2018613b7ae5b0
Rowanas <rowanatwork@hotmail.co.uk>
2022-05-30 11:15:55 +0100

    W8PR-1 Added domain and broad folder/file structure

---
+++ src/main/java/com/qa/W8PR/W8PRRecipeProjectApplication.java
@@ -0,0 +1,17 @@
+package com.qa.W8PR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class W8PRRecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W8PRRecipeProjectApplication.class, args);
+	}
+
+}
+
+
---
+++ src/main/java/com/qa/W8PR/domain/Recipe.java
@@ -0,0 +1,46 @@
+package com.qa.W8PR.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//creating a table with spring. Doesn't need to be called, SpringApplication goes and hunts all of the springable stuff
+//and pops it in your tables.
+//Lombok used as proof of Lombok use. I used Lombok.
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recipe {
+
+	@Id // makes this column the primary key you can put it in another non-numerical key!
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //longwinded way of saying "autoincrement"
+	private long id;
+	
+	//may put "unique = true" again or even make it my primary key as a stretch goal
+	//with functionality to kick a user to check an existing recipe if they attempt to create
+	//another with the same name.
+	@Column(nullable = false)
+	private String recipeName; // recognises camelCase and split to become SQLable names
+	
+	@Column(nullable = false)
+	private String ingredients;
+
+	@Column
+	private boolean dietFriendly;
+
+	public Recipe(String recipeName, String ingredients, boolean dietFriendly) {
+		super();
+		this.recipeName = recipeName;
+		this.ingredients = ingredients;
+		this.dietFriendly = dietFriendly;
+	}	
+	}
